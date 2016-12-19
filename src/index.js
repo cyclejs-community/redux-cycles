7f3f44ed4814e62cbc6e5d492324c051857c2905
Nick Balestra <nbalestra@opentable.com>
2016-12-19 11:36:47 +0000

    added combineCycles util

--- src/index.js
+++ src/index.js
@@ -63,3 +63,25 @@ export function createCycleMiddleware(mainFn, drivers) {
     }
   }
 }
+
+export function combineCycles(...mains) {
+  return sources => {
+    const sinks = mains.map(main => main(sources));
+
+    const drivers = Object.keys(
+      sinks.reduce((drivers, sink) => Object.assign(drivers, sink), {})
+    );
+
+    const combinedSinks = drivers
+      .reduce((combinedSinks, driver) => {
+        const driverSinks = sinks
+          .filter(sink => sink[driver])
+          .map(sink => sink[driver]);
+
+        combinedSinks[driver] = xs.merge(...driverSinks);
+        return combinedSinks;
+      }, {});
+
+    return combinedSinks;
+  }
+}
