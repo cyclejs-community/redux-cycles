877ca9a1cfef2c8da3eada693e3ab27846b32fef
lmatteis <lmatteis@gmail.com>
2016-12-28 00:39:21 +0100

    Add assertSourcesSinks test util

--- example/cycle/test/test.js
+++ example/cycle/test/test.js
@@ -1,4 +1,3 @@
-
 import assert from 'assert';
 import xs from 'xstream';
 import fromDiagram from 'xstream/extra/fromDiagram';
@@ -7,64 +6,158 @@ import * as actions from '../../actions';
 
 import { fetchReposByUser } from '../';
 
+function assertEqualStream(actual, expected, done) {
+  let calledComplete = 0;
+  let completeStore = {};
+
+  function complete (label, entries) {
+    calledComplete++;
+
+    completeStore[label] = entries;
+
+    if (calledComplete === 2) {
+      assert.deepEqual(completeStore['actual'], completeStore['expected']);
+      done()
+    }
+  }
+  const completeListener = (label) => {
+    const entries = [];
+
+    return {
+      next (ev) {
+        entries.push({type: 'next', value: ev});
+      },
+
+      complete () {
+        entries.push({type: 'complete'});
+
+        complete(label, entries)
+      },
+
+      error (error) {
+        entries.push({type: 'error', error});
+
+        complete(label, entries);
+      }
+    }
+  }
+  actual.addListener(completeListener('actual'))
+  expected.addListener(completeListener('expected'))
+}
+
+function assertSourcesSinks(sources, sinks, main, done) {
+  const _sources = Object.keys(sources)
+    .reduce((_sources, sourceKey) => {
+      const sourceObj = sources[sourceKey];
+      const diagram = Object.keys(sourceObj)[0];
+      const sourceOpts = sourceObj[diagram];
+
+      let obj = {};
+      if (sourceOpts.values) {
+        obj = {
+          [sourceKey]: fromDiagram(diagram, sourceOpts)
+        }
+      } else {
+        // it's a function
+        let firstKey = Object.keys(sourceOpts)[0];
+        obj = {
+          [sourceKey]: {
+            [firstKey]: () => fromDiagram(diagram, sourceOpts[firstKey]())
+          }
+        }
+      }
+      return Object.assign(_sources, obj);
+    }, {})
+
+  const _sinks = Object.keys(sinks)
+    .reduce((_sinks, sinkKey) => {
+      const sinkObj = sinks[sinkKey];
+      const diagram = Object.keys(sinkObj)[0];
+      const sinkOpts = sinkObj[diagram];
+
+      return Object.assign(_sinks, { [sinkKey]: fromDiagram(diagram, sinkOpts) });
+    }, {});
+
+  const _main = main(_sources);
+
+  Object.keys(sinks)
+    .map(sinkKey => assertEqualStream(_main[sinkKey], _sinks[sinkKey], done));
+}
+
 describe('Cycles', function() {
   describe('fetchReposByUser', function() {
-    it('should emit HTTP request given ACTION, and should emit ACTION given HTTP response', function(done) {
+    it('should emit HTTP requests given ACTIONs', function(done) {
       const user1 = 'lmatteis';
+      const user2 = 'luca';
+
+      const actionSourcesOpts = {
+        values: {
+          a: actions.requestReposByUser(user1),
+          b: actions.requestReposByUser(user2)
+        }
+      };
+
+      const httpSourcesOpts = {
+        select: () => null
+      };
+
+      const httpSinksOpts = {
+        values: {
+          x: {
+            url: `https://api.github.com/users/${user1}/repos`,
+            category: 'users'
+          },
+          y: {
+            url: `https://api.github.com/users/${user2}/repos`,
+            category: 'users'
+          }
+        }
+      };
+
+      // Asserts that the sources, trigger the provided sinks,
+      // when executing the fetchReposByUser function
+      assertSourcesSinks({
+        ACTION: { 'ab|': actionSourcesOpts },
+        HTTP:   { '--|': httpSourcesOpts }
+      }, {
+        HTTP:   { 'xy|': httpSinksOpts }
+      }, fetchReposByUser, done);
+
+    });
+
+    it('should emit ACTION given HTTP response', function(done) {
+      const user1 = 'lmatteis';
+      const user2 = 'luca';
+
       const response = { body: { foo: 'bar' } };
 
-      const ACTION = fromDiagram('a-|', {
+      const actionSourcesOpts = {
         values: {
           a: actions.requestReposByUser(user1)
         }
-      });
+      };
 
-      const HTTP = {
-        select: () => fromDiagram('-a|', {
+      const httpSourcesOpts = {
+        select: () => ({
           values: {
-            a: xs.of(response)
+            r: xs.of(response)
           }
         })
-      }
+      };
 
-      const sinks = fetchReposByUser({
-        ACTION,
-        HTTP
-      });
-
-      const ACTIONexpected = [
-        actions.receiveUserRepos(user1, response.body)
-      ];
-
-      const HTTPexcepted = [
-        {
-          url: `https://api.github.com/users/${user1}/repos`,
-          category: 'users'
-        },
-      ];
-
-      var stack = 2;
-      sinks.HTTP.addListener({
-        next (value) {
-          assert.deepEqual(value, HTTPexcepted.shift())
-        },
-
-        complete () {
-          stack--;
-          if (stack == 0) done()
+      const actionSinksOpts = {
+        values: {
+          a: actions.receiveUserRepos(user1, response.body)
         }
-      });
+      };
 
-      sinks.ACTION.addListener({
-        next (value) {
-          assert.deepEqual(value, ACTIONexpected.shift())
-        },
+      assertSourcesSinks({
+        ACTION: { 'a|': actionSourcesOpts },
+        HTTP:   { 'r|': httpSourcesOpts }
+      }, {
+        ACTION: { 'a|': actionSinksOpts }
+      }, fetchReposByUser, done);
 
-        complete () {
-          stack--;
-          if (stack == 0) done()
-        }
-      });
     });
   });
 });
