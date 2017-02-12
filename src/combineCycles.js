84137ec34fa08b26629abc9a0f76865c6b003733
lmatteis <lmatteis@gmail.com>
2017-02-12 15:06:30 +0100

    Add ESLint

--- src/combineCycles.js
+++ src/combineCycles.js
@@ -1,23 +1,23 @@
-import xs from 'xstream';
+import xs from 'xstream'
 
 export default function combineCycles(...mains) {
   return sources => {
-    const sinks = mains.map(main => main(sources));
+    const sinks = mains.map(main => main(sources))
 
     const drivers = Object.keys(
       sinks.reduce((drivers, sink) => Object.assign(drivers, sink), {})
-    );
+    )
 
     const combinedSinks = drivers
       .reduce((combinedSinks, driver) => {
         const driverSinks = sinks
           .filter(sink => sink[driver])
-          .map(sink => sink[driver]);
+          .map(sink => sink[driver])
 
-        combinedSinks[driver] = xs.merge(...driverSinks);
-        return combinedSinks;
-      }, {});
+        combinedSinks[driver] = xs.merge(...driverSinks)
+        return combinedSinks
+      }, {})
 
-    return combinedSinks;
+    return combinedSinks
   }
 }
--- test/createCycleMiddleware.test.js
+++ test/createCycleMiddleware.test.js
@@ -1,16 +1,17 @@
-import { createCycleMiddleware } from '../';
-import { createStore, applyMiddleware } from 'redux';
-import xs from 'xstream';
-jest.useFakeTimers();
+/* eslint-disable no-undef */
+import { createCycleMiddleware } from '../'
+import { createStore, applyMiddleware } from 'redux'
+import xs from 'xstream'
+jest.useFakeTimers()
 
 function initStore(main, drivers, reducer = null) {
-  const rootReducer = reducer || ((state = [], action) => state.concat(action));
-  const cycleMiddleware = createCycleMiddleware(main, drivers);
+  const rootReducer = reducer || ((state = [], action) => state.concat(action))
+  const cycleMiddleware = createCycleMiddleware(main, drivers)
   const store = createStore(
     rootReducer,
     applyMiddleware(cycleMiddleware)
-  );
-  return store;
+  )
+  return store
 }
 
 describe('Redux cycle middleware', () => {
@@ -18,7 +19,7 @@ describe('Redux cycle middleware', () => {
     function main(sources) {
       const pong$ = sources.ACTION
         .filter(action => action.type === 'PING')
-        .mapTo({ type: 'PONG' });
+        .mapTo({ type: 'PONG' })
 
       return {
         ACTION: pong$
@@ -36,19 +37,19 @@ describe('Redux cycle middleware', () => {
 
     expect(store.getState()).toMatchObject(expectedActions)
 
-    done();
+    done()
   })
 
   it('dispatches a PING to see whether the middleware dispatches a PONG after 10 seconds', (done) => {
     function main(sources) {
       const pong$ = sources.ACTION
         .filter(action => action.type === 'PING')
-        .map(a =>
+        .map(() =>
             xs.periodic(10000)
                 .take(1)
                 .mapTo({ type: 'PONG' })
         )
-        .flatten();
+        .flatten()
 
       return {
         ACTION: pong$
@@ -69,36 +70,36 @@ describe('Redux cycle middleware', () => {
       { type: 'PING' }
     ])
 
-    expect(store.getState()).not.toMatchObject(expectedActions);
-    jest.runAllTimers();
-    expect(setInterval.mock.calls[0][1]).toBe(10000);
-    expect(store.getState()).toMatchObject(expectedActions);
-    done();
+    expect(store.getState()).not.toMatchObject(expectedActions)
+    jest.runAllTimers()
+    expect(setInterval.mock.calls[0][1]).toBe(10000)
+    expect(store.getState()).toMatchObject(expectedActions)
+    done()
   })
 
   it('dispatches INCREMENT_ASYNC and INCREMENT_IF_ODD actions to check whether state updates correctly', (done) => {
     function main(sources) {
-      const state$ = sources.STATE;
+      const state$ = sources.STATE
       const isOdd$ = state$
         .map(state => state % 2 === 1)
-        .take(1);
+        .take(1)
 
       const incrementIfOdd$ = sources.ACTION
         .filter(action => action.type === 'INCREMENT_IF_ODD')
-        .map(action =>
+        .map(() =>
           isOdd$
         )
         .flatten()
         .filter(isOdd => isOdd)
-        .mapTo({ type: 'INCREMENT' });
+        .mapTo({ type: 'INCREMENT' })
 
       const increment$ = sources.ACTION
         .filter(action => action.type === 'INCREMENT_ASYNC')
-        .mapTo({ type: 'INCREMENT' });
+        .mapTo({ type: 'INCREMENT' })
 
       const decrement$ = sources.ACTION
         .filter(action => action.type === 'DECREMENT_ASYNC')
-        .mapTo({ type: 'DECREMENT' });
+        .mapTo({ type: 'DECREMENT' })
 
       const both$ = xs.merge(increment$, decrement$)
 
@@ -109,29 +110,29 @@ describe('Redux cycle middleware', () => {
 
     const store = initStore(main, {}, (state = 0, action) => {
       switch (action.type) {
-        case 'INCREMENT':
-          return state + 1;
-        case 'DECREMENT':
-          return state - 1;
-        default:
-          return state;
+      case 'INCREMENT':
+        return state + 1
+      case 'DECREMENT':
+        return state - 1
+      default:
+        return state
       }
     })
 
     store.dispatch({ type: 'INCREMENT_ASYNC' })
-    expect(store.getState()).toBe(1);
+    expect(store.getState()).toBe(1)
     store.dispatch({ type: 'INCREMENT_ASYNC' })
-    expect(store.getState()).toBe(2);
+    expect(store.getState()).toBe(2)
     store.dispatch({ type: 'INCREMENT_ASYNC' })
-    expect(store.getState()).toBe(3);
+    expect(store.getState()).toBe(3)
     store.dispatch({ type: 'INCREMENT_IF_ODD' })
-    expect(store.getState()).toBe(4);
+    expect(store.getState()).toBe(4)
     store.dispatch({ type: 'INCREMENT_IF_ODD' })
-    expect(store.getState()).toBe(4);
+    expect(store.getState()).toBe(4)
     store.dispatch({ type: 'INCREMENT_ASYNC' })
-    expect(store.getState()).toBe(5);
+    expect(store.getState()).toBe(5)
 
-    done();
+    done()
 
   })
 })
